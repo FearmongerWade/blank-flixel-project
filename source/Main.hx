305730185e2295ae0f050068b4122329f6102108
Wade <84008574+FearmongerWade@users.noreply.github.com>
2024-10-25 15:36:24 +0200

    more organization aaahhhhh

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+
+		addChild(new FlxGame(0, 0, states.PlayState, 60, 60, true, false));
 	}
 }
